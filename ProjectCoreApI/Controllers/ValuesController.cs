5b59958a3fae60ff891e326bb1ac2c88bf3aff8d
Owner <Owner@DESKTOP-BMQUE5Q>
2023-10-18 15:40:04 -0600

    Add project files.

---
+++ ProjectCoreApI/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectCoreApI.Database;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectCoreApI.Controllers
+{
+    [Route("api/Values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ShoppyDbContext _context;
+
+        public ValuesController()
+        {
+            _context = new ShoppyDbContext();
+        }
+        // GET: api/<ValuesController>
+        [Route("~/api/GetUsers")]
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [Route("~/api/CreateUser")]
+        [HttpPost]
+        public int CreateUser(User user)
+        {
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user.UId;
+        }
+
+        //[Route("~/api/GetDetails")]
+        //public IEnumerable<Brand> GetDetails()
+        //{
+        //    return _context.Brands.ToList();
+        //}
+
+        //[Route("~/api/GetShoppingDetails")]
+        //public IEnumerable<Item> GetShoppingDetails()
+        //{
+        //    return _context.Items.ToList();
+
+        //}
+
+    }
+}
---
+++ ProjectCoreApI/Database/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectCoreApI.Database;
+
+public partial class User
+{
+    public int UId { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+}
